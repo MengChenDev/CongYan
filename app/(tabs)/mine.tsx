829af6ffec0ace069ff2980a9230b72fa9a4dda4
MengChen <tatsukimengchen@163.com>
2024-12-10 18:21:04 +0800

    feature: 加入登出功能

--- app/(tabs)/mine.tsx
+++ app/(tabs)/mine.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Logout } from "@/utils/user";
+import { Button, Layout } from "@ui-kitten/components";
+import { router } from "expo-router";
+import React from "react";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
 
 const Mine = () => {
+  const handleLogout = () => {
+    Logout();
+    router.replace("/");
+  };
   return (
-    <View>
-      <Text>Mine</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-[#F7F9FC] h-full">
+      <ScrollView>
+        <Layout className="p-4 h-full w-full overflow-y-auto" level="2">
+          <Button appearance="outline" status="danger" onPress={handleLogout}>
+            登出
+          </Button>
+        </Layout>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-export default Mine
+export default Mine;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
